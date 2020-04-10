b21d9129d7540fa9789b64f61a802aa2c2865960
root <root@pop-os.localdomain>
2020-04-10 20:11:56 +0200

    bigM to bigm

--- fabric/scripts/joinTechChannelForn2.sh
+++ fabric/scripts/joinTechChannelForn2.sh
@@ -3,7 +3,7 @@
 # export CORE_PEER_TLS_ENABLED=true
 
 # export ORDERER_CA=/fabric/organizations/ordererOrganizations/bacheca.com/orderers/orderer.bacheca.com/msp/tlscacerts/tlsca.bacheca.com-cert.pem
-# export BIGMARKET_RICHIEDENTE_CA=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/peers/bigMarket.richiedente.bacheca.com/tls/ca.crt
+# export BIGMARKET_RICHIEDENTE_CA=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/peers/bigmarket.richiedente.bacheca.com/tls/ca.crt
 # export TECH_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/tech.forn1.bacheca.com/tls/ca.crt
 # export TECH_FORN2_CA=/fabric/organizations/peerOrganizations/forn2.bacheca.com/peers/tech.forn2.bacheca.com/tls/ca.crt
 
