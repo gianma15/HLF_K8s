1feb3c7a1b94132bc35c86a13f850b78177f466d
root <root@pop-os.localdomain>
2020-04-11 16:19:00 +0200

    joins

--- fabric/scripts/clean.sh
+++ fabric/scripts/clean.sh
@@ -1,6 +1,4 @@
 rm -r organizations/peerOrganizations 
 rm -r organizations/ordererOrganizations 
 rm -r channel-artifacts
-docker kill $(docker ps -a -q)
-docker rm $(docker ps -a -q)
 reset
--- fabric/scripts/joinTechChannelForn2.sh
+++ fabric/scripts/joinTechChannelForn2.sh
@@ -1,16 +1,16 @@
-# export PATH=/fabric/../bin:/fabric:$PATH
-# export FABRIC_CFG_PATH=/fabric/config
-# export CORE_PEER_TLS_ENABLED=true
+export FABRIC_CFG_PATH=/fabric/config
 
-# export ORDERER_CA=/fabric/organizations/ordererOrganizations/bacheca.com/orderers/orderer.bacheca.com/msp/tlscacerts/tlsca.bacheca.com-cert.pem
-# export BIGMARKET_RICHIEDENTE_CA=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/peers/bigmarket-richiedente-bacheca-com/tls/ca.crt
-# export TECH_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/tech-forn1-bacheca-com/tls/ca.crt
-# export TECH_FORN2_CA=/fabric/organizations/peerOrganizations/forn2.bacheca.com/peers/tech-forn2-bacheca-com/tls/ca.crt
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=/fabric/organizations/ordererOrganizations/bacheca.com/orderers/orderer.bacheca.com/msp/tlscacerts/tlsca.bacheca.com-cert.pem
+export BIGMARKET_RICHIEDENTE_CA=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/peers/bigmarket-richiedente-bacheca-com/tls/ca.crt
+export TECH_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/tech-forn1-bacheca-com/tls/ca.crt
+export TECH_FORN2_CA=/fabric/organizations/peerOrganizations/forn2.bacheca.com/peers/tech-forn2-bacheca-com/tls/ca.crt
 
-# export CORE_PEER_LOCALMSPID="Forn2MSP"
-# export CORE_PEER_TLS_ROOTCERT_FILE=$TECH_FORN2_CA
-# export CORE_PEER_MSPCONFIGPATH=/fabric/organizations/peerOrganizations/forn2.bacheca.com/users/Admin@forn2.bacheca.com/msp
-# export CORE_PEER_ADDRESS=localhost:10051
+export ORDERER_URL=orderer-bacheca-com:7050
+export CORE_PEER_LOCALMSPID="Forn2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$TECH_FORN2_CA
+export CORE_PEER_MSPCONFIGPATH=/fabric/organizations/peerOrganizations/forn2.bacheca.com/users/Admin@forn2.bacheca.com/msp
+export CORE_PEER_ADDRESS=tech-forn2-bacheca-com:10051
 MAX_RETRY=5
 DELAY=3
 
