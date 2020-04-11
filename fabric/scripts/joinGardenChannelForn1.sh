00afe65e57cb948ab1af01f7a93cd99355813731
root <root@pop-os.localdomain>
2020-04-11 19:51:50 +0200

    orderer . to -

--- fabric/scripts/joinGardenChannelForn1.sh
+++ fabric/scripts/joinGardenChannelForn1.sh
@@ -1,7 +1,7 @@
 export FABRIC_CFG_PATH=/fabric/config
 
 export CORE_PEER_TLS_ENABLED=true
-export ORDERER_CA=/fabric/organizations/ordererOrganizations/bacheca.com/orderers/orderer.bacheca.com/msp/tlscacerts/tlsca.bacheca.com-cert.pem
+export ORDERER_CA=/fabric/organizations/ordererOrganizations/bacheca.com/orderers/orderer-bacheca-com/msp/tlscacerts/tlsca.bacheca.com-cert.pem
 export BIGMARKET_RICHIEDENTE_CA=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/peers/bigmarket-richiedente-bacheca-com/tls/ca.crt
 export GARDEN_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/garden-forn1-bacheca-com/tls/ca.crt
 
