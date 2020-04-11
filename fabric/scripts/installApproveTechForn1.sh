615ca63ddb6d78b4b1a2d2664168ba1550ef0912
root <root@pop-os.localdomain>
2020-04-11 18:40:03 +0200

    avoglia

--- fabric/scripts/installApproveTechForn1.sh
+++ fabric/scripts/installApproveTechForn1.sh
@@ -3,16 +3,18 @@ export FABRIC_CFG_PATH=/fabric/config
 export CORE_PEER_TLS_ENABLED=true
 export ORDERER_CA=/fabric/organizations/ordererOrganizations/bacheca.com/orderers/orderer.bacheca.com/msp/tlscacerts/tlsca.bacheca.com-cert.pem
 export BIGMARKET_RICHIEDENTE_CA=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/peers/bigmarket-richiedente-bacheca-com/tls/ca.crt
-export GARDEN_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/garden-forn1-bacheca-com/tls/ca.crt
+export TECH_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/tech-forn1-bacheca-com/tls/ca.crt
+export TECH_FORN2_CA=/fabric/organizations/peerOrganizations/forn2.bacheca.com/peers/tech-forn2-bacheca-com/tls/ca.crt
 
 export ORDERER_URL=orderer-bacheca-com:7050
 export CORE_PEER_LOCALMSPID="Forn1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$GARDEN_FORN1_CA
+export CORE_PEER_TLS_ROOTCERT_FILE=$TECH_FORN1_CA
 export CORE_PEER_MSPCONFIGPATH=/fabric/organizations/peerOrganizations/forn1.bacheca.com/users/Admin@forn1.bacheca.com/msp
-export CORE_PEER_ADDRESS=garden-forn1-bacheca-com:9051
+export CORE_PEER_ADDRESS=tech-forn1-bacheca-com:8051
 
 peer lifecycle chaincode install offerta.tar.gz
 peer lifecycle chaincode install richiesta.tar.gz
+peer lifecycle chaincode queryinstalled
 
 export CC_PACKAGE_ID_R=$1
 export CC_PACKAGE_ID_O=$2
--- fabric/scripts/installApproveTechForn2.sh
+++ fabric/scripts/installApproveTechForn2.sh
@@ -4,18 +4,20 @@ export CORE_PEER_TLS_ENABLED=true
 export ORDERER_CA=/fabric/organizations/ordererOrganizations/bacheca.com/orderers/orderer.bacheca.com/msp/tlscacerts/tlsca.bacheca.com-cert.pem
 export BIGMARKET_RICHIEDENTE_CA=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/peers/bigmarket-richiedente-bacheca-com/tls/ca.crt
 export TECH_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/tech-forn1-bacheca-com/tls/ca.crt
-export GARDEN_FORN1_CA=/fabric/organizations/peerOrganizations/forn1.bacheca.com/peers/garden-forn1-bacheca-com/tls/ca.crt
 export TECH_FORN2_CA=/fabric/organizations/peerOrganizations/forn2.bacheca.com/peers/tech-forn2-bacheca-com/tls/ca.crt
 
 export ORDERER_URL=orderer-bacheca-com:7050
-export CORE_PEER_LOCALMSPID="RichiedenteMSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=$BIGMARKET_RICHIEDENTE_CA
-export CORE_PEER_MSPCONFIGPATH=/fabric/organizations/peerOrganizations/richiedente.bacheca.com/users/Admin@richiedente.bacheca.com/msp
-export CORE_PEER_ADDRESS=bigmarket-richiedente-bacheca-com:7051
+export CORE_PEER_LOCALMSPID="Forn2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$TECH_FORN2_CA
+export CORE_PEER_MSPCONFIGPATH=/fabric/organizations/peerOrganizations/forn2.bacheca.com/users/Admin@forn2.bacheca.com/msp
+export CORE_PEER_ADDRESS=tech-forn2-bacheca-com:10051
 
+peer lifecycle chaincode install offerta.tar.gz
+peer lifecycle chaincode install richiesta.tar.gz
+peer lifecycle chaincode queryinstalled
 
-export CC_PACKAGE_ID_R=richiesta:200b6b42d71d6e24e2029b2477ee1dacf38402e7fc9185c4d1fa858d29b144ad
-export CC_PACKAGE_ID_O=offerta:444f2e109f37d2d9b3f8da7cd5bd43b6a5ad20939cd6c1e95eca9096e5d08751
+export CC_PACKAGE_ID_R=$1
+export CC_PACKAGE_ID_O=$2
 
+peer lifecycle chaincode approveformyorg -o $ORDERER_URL --ordererTLSHostnameOverride orderer.bacheca.com --channelID techchannel --name richiesta --version 1.0 --init-required --package-id $CC_PACKAGE_ID_R --sequence 1 --tls true --cafile $ORDERER_CA
 peer lifecycle chaincode approveformyorg -o $ORDERER_URL --ordererTLSHostnameOverride orderer.bacheca.com --channelID techchannel --name offerta --version 1.0 --collections-config /fabric/chaincode/bacheca/offerta/collections_configT.json --package-id $CC_PACKAGE_ID_O --sequence 1 --tls true --cafile $ORDERER_CA
-peer lifecycle chaincode approveformyorg -o $ORDERER_URL --ordererTLSHostnameOverride orderer.bacheca.com --channelID gardenchannel --name offerta --version 1.0 --collections-config /fabric/chaincode/bacheca/offerta/collections_configG.json --package-id $CC_PACKAGE_ID_O --sequence 1 --tls true --cafile $ORDERER_CA
